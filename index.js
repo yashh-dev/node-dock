7e32a29d86f8a71490f366f689c52bf48492082f
yashh-dev <yashwanthkrishna5027@gmail.com>
2022-12-13 19:58:51 +0530

    docker compose config

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/',(req,res)=>{
-    res.send("<h2>Hi there!!</h2>");
+    res.send("<h1>Bonjour!!</h1>");
 })
 
 const port = process.env.PORT || 3000;
