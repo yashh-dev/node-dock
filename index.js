1e40ae3fda657ca135b70c4cd80f5db74db32be1
yashh-dev <yashwanthkrishna5027@gmail.com>
2022-12-14 09:04:12 +0530

    docker compose for prod and dev env

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan');
 
+app.use(morgan())
 app.get('/',(req,res)=>{
-    res.send("<h1>Bonjour!!</h1>");
+    res.send(`<h1>Bonjour!!!!</h1> you are in ${process.env.NODE_ENV} env`);
 })
 
 const port = process.env.PORT || 3000;
