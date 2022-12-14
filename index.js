84c44e9f07376047f87d8c1d4b270d81b7899489
yashh-dev <yashwanthkrishna5027@gmail.com>
2022-12-14 13:41:44 +0530

    mongo container

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express')
-const app = express()
+const app = express();
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const {MONGO_USER,MONGO_PASSWORD,MONGO_IP,MONGO_PORT} = require('./config/config')
 
-app.use(morgan())
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+mongoose.set({strictQuery:true})
+mongoose.connect(
+    mongoURL
+).then(()=>{
+    console.log('succesfully connected to mongodb');
+}).catch((e)=>{
+    console.log("There was a problem");
+    console.log(e);
+})
+app.use(morgan('tiny'))
 app.get('/',(req,res)=>{
-    res.send(`<h1>Bonjour!!!!</h1> you are in ${process.env.NODE_ENV} env`);
+    res.send(`<h1>Bonjour</h1> you are in ${process.env.NODE_ENV} env`);
 })
 
 const port = process.env.PORT || 3000;
