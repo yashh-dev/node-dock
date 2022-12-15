9e107be6021fede328e4adec8e403d7bb751a675
Yashwanth Krishna <76145601+yashh-dev@users.noreply.github.com>
2022-12-15 14:42:43 +0000

    user login

--- index.js
+++ index.js
@@ -3,9 +3,19 @@ const app = express();
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const postRouter = require("./routes/postRoutes")
+const userRouter = require("./routes/userRoutes")
+const {MONGO_USER,MONGO_PASSWORD,MONGO_IP,MONGO_PORT, SESSION_SECRET, REDIS_PORT, REDIS_URL} = require('./config/config')
 
-app.use(express.json( ))
-const {MONGO_USER,MONGO_PASSWORD,MONGO_IP,MONGO_PORT} = require('./config/config')
+const session = require('express-session')
+const redis = require("redis")
+
+let RedisStore = require("connect-redis")(session)
+let redisClient = new redis.createClient({
+    host : REDIS_URL,
+    port : REDIS_PORT
+})
+
+
 
 const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
 mongoose.set({strictQuery:true})
@@ -24,11 +34,33 @@ function connectRetry(){
 
 connectRetry()
 
+
+app.use(session({
+    store : new RedisStore({
+        client:redisClient
+    }),
+    secret : SESSION_SECRET,
+    cookie : {
+        name: 'cook',
+        httpOnly: true,
+        secure: false,
+        resave: false,
+        saveUninitialized : false,
+        maxAge : 30000*60
+    },
+    resave: false,
+    saveUninitialized : false,
+}))
+
+app.use(express.json( ))
+
+
 app.use(morgan('tiny'))
 app.get('/',(req,res)=>{
     res.send(`<h1>Bonjour</h1> you are in ${process.env.NODE_ENV} env`);
 })
 
+app.use("/api/v1/users",userRouter)
 app.use("/api/v1/posts",postRouter)
 
 const port = process.env.PORT || 3000;
